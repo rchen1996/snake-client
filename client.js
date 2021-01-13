238687ea5e9c17b0e3952a1568dcb0c8fca76006
Rebecca Chen <rebecca061996@gmail.com>
2021-01-13 17:54:51 +0000

    added an event handler for connect to write a message to the connection on connection

--- client.js
+++ client.js
@@ -11,11 +11,16 @@ const net = require('net');
    // interpret incoming data as text
    conn.setEncoding('utf8');
 
+   conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: RBC');
+   });
+
    conn.on('data', (data) => {
      console.log(data);
-   })
+   });
 
    return conn;
- }
- 
+ };
+
  module.exports = { connect }
