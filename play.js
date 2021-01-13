2280d0e51f4df58565b875c74faef3e71531f81e
Rebecca Chen <rebecca061996@gmail.com>
2021-01-13 18:14:55 +0000

    refactored to move stdin functions into their own file

--- play.js
+++ play.js
@@ -1,30 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting...');
 connect();
 
-/**
- * Setup User Interface
- * Specifically, so that we can handle user input via stdin
- * will be used to take in keyboard commands to control snake
- */
-
- const setupInput = function() {
-   // stdin configuration
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  // handling ctrl + c user input
-  stdin.on('data', handleUserInput)
-  return stdin;
- };
-
- const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
- };
-
- setupInput();
+setupInput();
