b42f5e1c8536363f11e4388d7804717f5b7dc4c2
Rebecca Chen <rebecca061996@gmail.com>
2021-01-13 18:26:13 +0000

    added functionality for moving snake using stdin

--- input.js
+++ input.js
@@ -4,22 +4,34 @@
  * will be used to take in keyboard commands to control snake
  */
 
+// Stores the active TCP connection object
+let connection;
+
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
+  } else if (key === 'w') {
+    connection.write("Move: up");
+  } else if (key === "a") {
+    connection.write("Move: left");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "d") {
+    connection.write("Move: right");
   }
  };
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   // stdin configuration
- const stdin = process.stdin;
- stdin.setRawMode(true);
- stdin.setEncoding('utf8');
- stdin.resume();
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
 
- // handling ctrl + c user input
- stdin.on('data', handleUserInput)
- return stdin;
+  // handling ctrl + c user input
+  stdin.on('data', handleUserInput)
+  return stdin;
 };
 
 module.exports = { setupInput };
